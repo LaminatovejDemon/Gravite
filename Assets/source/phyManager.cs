7d9bdac7ef39faf3381b99206005a21207e8d26a
Filip Pokorny <filip.pokorny@solfa.uk>
2016-12-10 01:08:58 +0000

    Initial commit

---
+++ Assets/source/phyManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class phyManager : MonoBehaviour {
+	List<phyObject> _objects;
+	private static phyManager _instance;
+	public static phyManager manager {
+		get {
+			if (_instance == null) {
+				_instance = new GameObject().AddComponent<phyManager>();
+				_instance.gameObject.name = "phyManager";
+			}
+			return _instance;
+		}
+	}
+
+	public void SpreadGravity(phyObject source){
+		if (_objects == null) {
+			_objects = new List<phyObject> ();
+		}
+		if (!_objects.Contains (source)) {
+			_objects.Add (source);
+		}
+
+		for (int i = 0; i < _objects.Count; ++i) {
+			if (source == _objects [i]) {
+				continue;
+			}
+			_objects [i].ApplyGravity (source);	
+		}
+	}
+
+	void Update(){
+	/*	string log_ = "Objects: ";
+		for (int i = 0; i < _objects.Count; ++i) {
+			log_ += _objects [i] + ", ";
+		}
+		Debug.Log (log_);*/
+	}
+}
---
+++ Assets/source/phyObject.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class phyObject : MonoBehaviour {
+
+	public float SelfRotation = 0.0f;
+	public float Mass = 1.0f;
+	// Sun: 1.989 * 10^30 ~ 1989000
+	// Moon: 7.345 * 10^22 ~ 0.07345
+	// Earth: 5.972 * 10^24 ~ 5.972
+
+	float _resultRotation = 0;
+	Vector3 _resultForces = Vector3.zero;
+
+	Vector3 _gravityForce = Vector3.zero;
+	Vector3 _inertiaForce = Vector3.zero;
+
+	void Update () {
+		phyManager.manager.SpreadGravity (this);
+
+		this.transform.position += _resultForces * 0.1f;
+		this.transform.Rotate (Vector3.up, _resultRotation * Time.deltaTime);
+
+		_resultRotation = SelfRotation;
+		_resultForces *= 0.7f;
+	}
+		
+	public void ApplyGravity(phyObject source){
+		Vector3 direction_ = (source.transform.position - gameObject.transform.position);
+		Vector3 tangent_ = new Vector3 (-direction_.z, 0, direction_.x).normalized;
+		float massProduct_ = Mass * source.Mass;
+		float force_ = 10.0f * massProduct_ / (0.1f + Mathf.Pow(direction_.magnitude, 2.0f));
+		_gravityForce = direction_.normalized * force_ / Mass;
+	
+		if (source._resultRotation != 0) {
+			_inertiaForce = tangent_ * source.SelfRotation * force_ * 5.0f / Mass;
+		}
+
+		_resultForces += _inertiaForce * Time.deltaTime;
+		_resultForces += _gravityForce * Time.deltaTime;
+	}
+
+	void OnDrawGizmos(){
+		Gizmos.color = Color.green;
+		Vector3 pos_ = transform.position + Vector3.up * transform.localScale.y * 0.5f;
+		Gizmos.DrawLine (pos_, pos_ + (_gravityForce * 0.1f));
+		Gizmos.color = Color.red;
+		Gizmos.DrawLine (pos_, pos_ + (_inertiaForce * 0.1f));
+		Gizmos.color = Color.blue;
+		Gizmos.DrawLine (pos_, pos_ + (_resultForces * 0.001f));
+	}
+}
