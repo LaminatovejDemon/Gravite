cfb0d496df0f7888176ad5b6d4abc7121cb4b030
Omitkovej Demon <aleszvolanek@aleszvolanek.cz>
2016-12-16 00:09:46 +0100

    updated script (accelerates with time held) missing ufo position conversion towards screen resolution

---
+++ Assets/scripts/ufo.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ufo : MonoBehaviour
+{
+    bool pressed = false;
+    float startime;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButton(0))
+        {
+            if (!pressed)
+                {
+                    startime = Time.time;
+                }
+            pressed = true;
+            
+
+           Vector3 dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) / Camera.main.orthographicSize;
+            
+            this.GetComponent<Rigidbody>().AddForce(dir * 20 * (startime - Time.deltaTime));
+
+            print(dir);
+
+        }
+
+        if  (Input.GetMouseButtonUp(0))
+        {
+            pressed = false;
+        }
+
+
+    }
+
+}
+
+
