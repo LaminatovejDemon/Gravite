cc52b1c23cf3fc789a617faf1ac7c787c18a7ce0
Filip Pokorny <filip.pokorny@solfa.uk>
2016-12-18 09:05:14 +0000

    android build setup, physics altered (only friction added to ufo 0.0f -> 1.0f), multitouch added

--- Assets/source/ufo.cs
+++ Assets/source/ufo.cs
@@ -7,17 +7,45 @@ public class ufo : MonoBehaviour
     bool pressed = false;
     float startime;
 
-    // Use this for initialization
-    void Start()
-    {
+	Vector3 _touchDirection = Vector3.zero;
 
+    // Use this for initialization
+    void OnDrawGizmos()
+    {
+		Gizmos.color = Color.green;
+		Gizmos.DrawLine (transform.position, transform.position + (_touchDirection.normalized * 50f));
     }
 
     // Update is called once per frame
     void Update()
     {
+		Vector3 accumulate_ = Vector3.zero;
 
-        if (Input.GetMouseButton(0))
+		if (Input.GetMouseButton (0)) {
+			pressed = true;
+		}
+		else if (Input.GetMouseButtonUp (0)) {
+			pressed = false;
+		}
+
+		if (pressed) {
+			accumulate_ += Input.mousePosition;
+		} else if (Input.touchCount > 0 ){
+			for (int i = 0; i < Input.touchCount; ++i) {
+				accumulate_ += new Vector3 (Input.GetTouch (i).position.x, 0, Input.GetTouch(i).position.y);
+			}
+			accumulate_ /= Input.touchCount;
+		}
+		else {
+			return;
+		}
+
+		Vector3 inputWorld_ = Camera.main.ScreenToWorldPoint (accumulate_);
+		_touchDirection = inputWorld_ - transform.position;
+		this.GetComponent<Rigidbody>().AddForce(_touchDirection * 10.0f);
+
+
+        /*if (Input.GetMouseButton(0))
         {
             if (!pressed)
                 {
@@ -52,7 +80,7 @@ public class ufo : MonoBehaviour
         {
             pressed = false;
         }
-
+*/
 
     }
 
